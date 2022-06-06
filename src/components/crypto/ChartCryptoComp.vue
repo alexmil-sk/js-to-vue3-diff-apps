aa7f433a17c158ecd877f9c1efabec2450cf4c6d
alexmil-sk <alexmlrdv@gmail.com>
2022-06-06 11:07:28 +0200

    several charts select

--- src/components/crypto/ChartCryptoComp.vue
+++ src/components/crypto/ChartCryptoComp.vue
@@ -2,7 +2,7 @@
   <div class="d-flex flex-column w-50 mt-4 border-top border-secondary">
     <div class="input-group d-flex flex-column mt-3  mb-3 w-75">
       <label for="coinInput" class="fs-5">Введите пару в формате: BTC-USDT</label>
-      <div class="d-flex">
+      <div class="d-flex align-items-end " style="height: 52px;">
         <input
             id="coinInput"
             type="text"
@@ -15,9 +15,10 @@
         />
 
         <button
-            class="btn btn-secondary btn-outline-secondary text-white"
+            class="btn btn-secondary btn-outline-secondary text-white text-uppercase"
             @click="getCryptoCoin"
-        >Подтвердить</button>
+        >Подтвердить
+        </button>
       </div>
     </div>
     <div
--- src/views/AllChartsView.vue
+++ src/views/AllChartsView.vue
@@ -2,28 +2,37 @@
   <div>
     <h1>Графики криптовалюты (BINANCE / Спот)</h1>
 
+    <ChartWindowQuantity @selectChartsQuantity="selectChartsQuantity"/>
+
 
     <div class=" d-inline-flex flex-wrap justify-content-center w-100 mt-3">
-      <ChartCryptoComp/>
-      <ChartCryptoComp/>
-      <ChartCryptoComp/>
-      <ChartCryptoComp/>
+          <ChartCryptoComp v-for=" (item, idx) in chartsQuantity" />
     </div>
+
   </div>
 </template>
 
 <script>
 import ChartCryptoComp from '@/components/crypto/ChartCryptoComp.vue';
-
+import ChartWindowQuantity from '@/components/crypto/ChartWindowQuantity.vue';
 
 export default {
   name: "all-charts-view",
   components: {
     ChartCryptoComp,
+    ChartWindowQuantity
   },
-
+  data() {
+    return {
+      chartsQuantity: 1,
+      visible: true,
+    }
+  },
   methods: {
-
+    selectChartsQuantity(n) {
+      this.chartsQuantity = Number(n);
+      this.visible = true;
+    },
 
   },
   mounted() {
@@ -35,6 +44,15 @@ export default {
 </script>
 
 <style scoped>
+
+.fade-enter-active,
+.fade-leave-active {
+  transition:   opacity 1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.fade-enter, .fade-leave-to
+  /* .slide-fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
 
 
 
